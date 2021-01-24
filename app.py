8584c991398e65734ce2959561c2c98f814d6d93
Roberto Cortez <cortezrobertov@gmail.com>
2021-01-23 18:19:11 -0600

    final

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ db = SQLAlchemy(app) # SQLAlchemy Object
 login_manager =  LoginManager(app)
 login_manager.login_view = "login"
 
-# # EMAIL
-# app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-# app.config['MAIL_PORT'] = 465
-# app.config['MAIL_USERNAME'] = 'todolist2021s@gmail.com'
-# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-# app.config['MAIL_USE_TLS'] = False
-# app.config['MAIL_USE_SSL'] = True
-# mail = Mail(app)
+# EMAIL
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'todolist2021s@gmail.com'
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 #DATA MODEL
 # USER
